f7ba64bd2415cdd2ab0df356c33a4f84fb42da56
Quentin Headen <qheaden@phaseshiftsoftware.com>
2015-02-13 02:47:49 -0500

    Added a seeder for events, and made all seeders truncate.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 
 use App\EventCategory;
 use App\User;
+use App\Event;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
@@ -17,7 +18,8 @@ class DatabaseSeeder extends Seeder {
 	{
 		$this->call('EventCategoriesSeeder');
     $this->call('UsersSeeder');
-    $this->command->info('Event Categories Table Created!');
+    $this->call('EventsSeeder');
+    $this->command->info('All Tables Seeded!');
 	}
 
 }
@@ -26,7 +28,7 @@ class EventCategoriesSeeder extends Seeder {
 
 	public function run()
 	{
-		DB::table('event_categories')->delete();
+		DB::table('event_categories')->truncate();
     EventCategory::create(array('name' => 'sports'));
     EventCategory::create(array('name' => 'entertainment'));
     EventCategory::create(array('name' => 'other'));
@@ -40,7 +42,7 @@ class UsersSeeder extends Seeder {
 
   public function run()
   {
-    DB::table('users')->delete();
+    DB::table('users')->truncate();
     User::create(array(
       'name' => 'Quentin Headen',
       'email' => 'qheaden@cs.odu.edu',
@@ -64,6 +66,40 @@ class UsersSeeder extends Seeder {
       'name' => 'Bharath Kongara',
       'email' => 'bkongara@cs.odu.edu',
       'password' => 'password'));
+  }
+}
+
+class EventsSeeder extends Seeder {
+
+  public function run()
+  {
+    DB::table('events')->truncate();
+    Event::create(array(
+      'category_id' => 0,
+      'title' => 'HackU3',
+      'description' => 'University hackathon event hosted by Dominion Enterprises.',
+      'location' => '150 Granby Street, Norfolk, VA 23510',
+      'time' => '2015-2-13',
+      'photo_path' => '/tmp',
+      'attendee_limit' => 60));
+    
+    Event::create(array(
+      'category_id' => 0,
+      'title' => 'ODU Hackathon',
+      'description' => 'Hackathon hosted by Old Dominion University.',
+      'location' => '5115 Hampton Blvd, Norfolk, VA 23529',
+      'time' => '2015-3-21',
+      'photo_path' => '/tmp',
+      'attendee_limit' => 0));
+
+    Event::create(array(
+      'category_id' => 0,
+      'title' => 'Pickup Basketball Game',
+      'description' => 'Fun pickup game in the student b-ball courts.',
+      'location' => 'ODU Basketball Court',
+      'time' => '2015-2-13',
+      'photo_path' => '/tmp',
+      'attendee_limit' => 10));
   }
 }
 
